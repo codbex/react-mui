530736056292ad3327e32e80ee84690a97513cbe
ThuF <jordan.p.pavlov@gmail.com>
2024-07-24 21:11:04 +0300

    Adds Floating Action Button

--- react-mui/FloatingActionButton.jsx
+++ react-mui/FloatingActionButton.jsx
@@ -1,30 +1,44 @@
 const {
     Box,
     Fab,
-} = MaterialUI
-const {
-    AddIcon,
-    EditIcon,
-    FavoriteIcon,
-    NavigationIcon
-} = MaterialUIIcons
+} = MaterialUI;
+
+// const {
+//     AddIcon,
+//     EditIcon,
+//     FavoriteIcon,
+//     NavigationIcon
+// } = MaterialUIIcons
 
 function FloatingActionButtons() {
     return (
         <Box sx={{ '& > :not(style)': { m: 1 } }}>
             <Fab color="primary" aria-label="add">
-                <AddIcon />
             </Fab>
             <Fab color="secondary" aria-label="edit">
-                <EditIcon />
             </Fab>
             <Fab variant="extended">
-                <NavigationIcon sx={{ mr: 1 }} />
                 Navigate
             </Fab>
             <Fab disabled aria-label="like">
-                <FavoriteIcon />
             </Fab>
         </Box>
     );
+    // return (
+    //     <Box sx={{ '& > :not(style)': { m: 1 } }}>
+    //         <Fab color="primary" aria-label="add">
+    //             <AddIcon />
+    //         </Fab>
+    //         <Fab color="secondary" aria-label="edit">
+    //             <EditIcon />
+    //         </Fab>
+    //         <Fab variant="extended">
+    //             <NavigationIcon sx={{ mr: 1 }} />
+    //             Navigate
+    //         </Fab>
+    //         <Fab disabled aria-label="like">
+    //             <FavoriteIcon />
+    //         </Fab>
+    //     </Box>
+    // );
 }
